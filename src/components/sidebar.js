fcec55884d926686baa971a119e866812cedd46b
key4127 <2827092384@qq.com>
2025-02-01 21:44:51 +0800

    complete part of login page

---
+++ src/components/sidebar.js
@@ -0,0 +1,10 @@
+import '../css/sidebar.css';
+
+function Sidebar(props) {
+  return (
+    <div className="basic-nav-bar">
+    </div>
+  )
+}
+
+export default Sidebar;
---
+++ src/pages/login.js
@@ -0,0 +1,44 @@
+import Sidebar from "../components/sidebar";
+import { Select, Input, Button } from "antd";
+import '../css/login.css';
+
+function InputName() {
+  return (
+    <div className="login-input-name">
+      <p>用户类型</p>
+      <p>用户名</p>
+      <p>密码</p>
+    </div>
+  )
+}
+
+function InputBar() {
+  return (
+    <div className="login-input-bar">
+      <Select
+        style={{ width: '192px' }}
+        options={[{ value: 'student', label: <span>学生</span> },
+                        { value: 'professor', label: <span>教师</span> },
+                        { value: 'administrator', label: <span>管理员</span> }]} />
+      <Input placeholder="" />
+      <Input placeholder="" />
+    </div>
+  )
+}
+
+function Login() {
+  return (
+    <div className="login">
+      <Sidebar />
+      <div className="login-container">
+        <div className="login-input-container">
+          <InputName />
+          <InputBar />
+        </div>
+        <Button className="login-button">登录</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Login;
