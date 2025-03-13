a71957a6e58e072fdb627f27838ee183811dcef0
key4127 <2827092384@qq.com>
2025-03-13 16:35:12 +0800

    merge front end and back end

---
+++ Canvas-back/src/main/java/canvas/CanvasBackApplication.java
@@ -0,0 +1,13 @@
+package canvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanvasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanvasBackApplication.class, args);
+	}
+
+}
---
+++ Canvas-back/src/test/java/canvas/CanvasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package canvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanvasBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
